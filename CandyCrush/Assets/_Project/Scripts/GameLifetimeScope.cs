21b0cb9975004d8f95dc231fc050e51366dfeed9
TheGabmeister <thegabmeister@protonmail.com>
2025-06-20 15:03:17 +0800

    Add CandyCrush

---
+++ CandyCrush/Assets/_Project/Scripts/GameLifetimeScope.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class GameLifetimeScope : LifetimeScope
+{
+    [SerializeField] HelloScreen helloScreen;
+    
+    protected override void Configure(IContainerBuilder builder)
+    {
+        builder.Register<HelloWorldService>(Lifetime.Singleton);
+        builder.RegisterEntryPoint<GamePresenter>();
+        builder.RegisterComponent(helloScreen);
+    }
+}
