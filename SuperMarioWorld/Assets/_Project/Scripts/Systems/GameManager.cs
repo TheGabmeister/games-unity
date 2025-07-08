66b6e1f28e92f058d297db8daac0365ea721dacd
TheGabmeister <thegabmeister@protonmail.com>
2025-07-08 20:06:29 +0800

    Rename SceneData to LevelData

--- SuperMarioWorld/Assets/_Project/Scripts/Systems/GameManager.cs
+++ SuperMarioWorld/Assets/_Project/Scripts/Systems/GameManager.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 using System.Collections;
 using UnityEditor;
 using UnityEngine.SceneManagement;
+using UnityServiceLocator;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject _playerPrefab;
-    [SerializeField] SceneData[] _sceneData;
+    [SerializeField] LevelData[] _levelData;
+    private int _timeRemaining = 10;
+
+    public event Action<int> TimerUpdated;
     
     private void OnEnable()
     {
@@ -17,6 +21,11 @@ public class GameManager : MonoBehaviour
     {
 
     }
+
+    void Awake()
+    {
+        ServiceLocator.Global.Register<GameManager>(this);
+    }
     
     void Start()
     {
@@ -25,14 +34,21 @@ public class GameManager : MonoBehaviour
         // Because the type SceneReference cannot be checked for equality, we instead
         // iterate over the names of each SceneReference and compare that.
         string startScene = SceneManager.GetActiveScene().name;
-        foreach (var s in _sceneData)
+        foreach (var data in _levelData)
         {
-            if (s.sceneName.Name == startScene && s.sceneType == SceneType.Gameplay)
+            if (data.levelName.Name == startScene && data.levelType == LevelType.Gameplay)
             {
                 Instantiate(_playerPrefab, GetPlayerStartPosition(), Quaternion.identity);
             }
         }
 #endif
+
+        
+    }
+
+    void StartTimer()
+    {
+        InvokeRepeating(nameof(DeductTimer), 0f, 1f);
     }
     
     Vector3 GetPlayerStartPosition()
@@ -45,5 +61,20 @@ public class GameManager : MonoBehaviour
             return new Vector3(cameraPos.x, cameraPos.y, 0);
         }
         return playerStart.transform.position;
+    }
+
+    void DeductTimer()
+    {
+        _timeRemaining--;
+        TimerUpdated?.Invoke(_timeRemaining);
+        if (_timeRemaining <= 0)
+        {
+            StartGameOverSequence();
+        }
+    }
+
+    void StartGameOverSequence()
+    {
+        
     }
 }
