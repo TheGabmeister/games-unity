fe1a5cd0c5130d5485f4d28ece1d5b37bbb28a6e
TheGabmeister <thegabmeister@protonmail.com>
2025-12-03 19:26:25 +0800

    Fix compile error

--- SuperMario/Assets/_Project/SO_Data/Worlds/SceneDataVariable.cs
+++ SuperMario/Assets/_Project/SO_Data/Worlds/SceneDataVariable.cs
@@ -10,7 +10,7 @@ namespace ScriptableObjectArchitecture
         fileName = "SceneDataVariable.asset",
         menuName = SOArchitecture_Utility.VARIABLE_SUBMENU + "SceneData",
         order = SOArchitecture_Utility.ASSET_MENU_ORDER_COLLECTIONS + 2)]
-    public sealed class SceneDataVariable : BaseVariable<SceneData, SceneDataEvent>
+    public sealed class SceneDataVariable : BaseVariable<SceneData>
     {
     } 
 }
