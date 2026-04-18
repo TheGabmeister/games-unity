10dfbb3d279e7f65276f3b2bf0b24068986b6620
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-04-18 10:34:26 +0800

    Fix loading screen

--- MegamanX4/Assets/_Project/Scripts/Services/LoadingScreen.cs
+++ MegamanX4/Assets/_Project/Scripts/Services/LoadingScreen.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class LoadingScreen : MonoBehaviour
 {
-    void Awake() => Hide();
-
     public void Show() => gameObject.SetActive(true);
     public void Hide() => gameObject.SetActive(false);
 }
