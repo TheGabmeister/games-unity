d05f174d255b713d64d8c59b3c11acb5dc5e9daf
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-04-23 20:15:03 +0800

    Refactor SceneLoader

--- DigimonWorld1/Assets/_Project/Scripts/GameManager.cs
+++ DigimonWorld1/Assets/_Project/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using Eflatun.SceneReference;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -10,6 +11,8 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] private SceneReference _gameplayScene;
     [SerializeField] private ZoneData _startingZone;
     [SerializeField] private ScreenFader _screenFader;
+
+    private ZoneData _currentZone;
 
     public async void LoadSplashscreenScene()
     {
@@ -34,14 +37,50 @@ public class GameManager : Singleton<GameManager>
     public async void LoadGameplayScene()
     {
         await _screenFader.FadeOut();
-        await SceneLoader.Instance.LoadScenes(_gameplayScene, _startingZone.Scene);
+        UnloadNonBootstrapScenes();
+        await SceneLoader.Instance.LoadScene(_gameplayScene);
+        await SceneLoader.Instance.LoadScene(_startingZone.Scene);
+        _currentZone = _startingZone;
+        ApplyCameraPosition(_currentZone);
+        await _screenFader.FadeIn();
+    }
+
+    public async void LoadZone(ZoneData zone)
+    {
+        if (_currentZone == zone) return;
+
+        await _screenFader.FadeOut();
+        if (_currentZone != null)
+            await SceneLoader.Instance.UnloadScene(_currentZone.Scene);
+        await SceneLoader.Instance.LoadScene(zone.Scene);
+        _currentZone = zone;
+        ApplyCameraPosition(_currentZone);
         await _screenFader.FadeIn();
     }
 
     private async Awaitable LoadSceneWithFade(SceneReference scene)
     {
         await _screenFader.FadeOut();
+        UnloadNonBootstrapScenes();
         await SceneLoader.Instance.LoadScene(scene);
+        _currentZone = null;
         await _screenFader.FadeIn();
+    }
+
+    private void UnloadNonBootstrapScenes()
+    {
+        for (int i = SceneManager.sceneCount - 1; i >= 0; i--)
+        {
+            Scene loaded = SceneManager.GetSceneAt(i);
+            if (loaded.buildIndex != 0)
+                SceneManager.UnloadSceneAsync(loaded);
+        }
+    }
+
+    private void ApplyCameraPosition(ZoneData zone)
+    {
+        GameplayCamera cam = FindFirstObjectByType<GameplayCamera>();
+        if (cam != null)
+            cam.transform.position = zone.CameraPosition;
     }
 }
--- DigimonWorld1/Assets/_Project/Scripts/SceneLoader.cs
+++ DigimonWorld1/Assets/_Project/Scripts/SceneLoader.cs
@@ -11,34 +11,20 @@ public class SceneLoader : Singleton<SceneLoader>
     public async Awaitable LoadScene(SceneReference scene)
     {
         OnSceneLoadStarted?.Invoke();
-        UnloadNonBootstrapScenes();
-        await LoadSceneAsync(scene);
-        OnSceneLoadCompleted?.Invoke();
-    }
-
-    public async Awaitable LoadScenes(params SceneReference[] scenes)
-    {
-        OnSceneLoadStarted?.Invoke();
-        UnloadNonBootstrapScenes();
-        foreach (var scene in scenes)
-            await LoadSceneAsync(scene);
-        OnSceneLoadCompleted?.Invoke();
-    }
-
-    private void UnloadNonBootstrapScenes()
-    {
-        for (int i = SceneManager.sceneCount - 1; i >= 0; i--)
-        {
-            Scene loaded = SceneManager.GetSceneAt(i);
-            if (loaded.buildIndex != 0)
-                SceneManager.UnloadSceneAsync(loaded);
-        }
-    }
-
-    private async Awaitable LoadSceneAsync(SceneReference scene)
-    {
         AsyncOperation op = SceneManager.LoadSceneAsync(scene.Path, LoadSceneMode.Additive);
         while (!op.isDone)
             await Awaitable.NextFrameAsync();
+        OnSceneLoadCompleted?.Invoke();
+    }
+
+    public async Awaitable UnloadScene(SceneReference scene)
+    {
+        Scene loaded = SceneManager.GetSceneByPath(scene.Path);
+        if (loaded.isLoaded)
+        {
+            AsyncOperation op = SceneManager.UnloadSceneAsync(loaded);
+            while (!op.isDone)
+                await Awaitable.NextFrameAsync();
+        }
     }
 }
